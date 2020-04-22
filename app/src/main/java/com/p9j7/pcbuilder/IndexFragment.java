02ef4b846570f80e0ee1feb32ce28df0f534f1b7
Ynight <vae1986s>
2020-04-22 13:47:41 +0800

    recyclerview布局 todo:图片的线性布局显示

---
+++ app/src/main/java/com/p9j7/pcbuilder/IndexFragment.java
@@ -0,0 +1,67 @@
+package com.p9j7.pcbuilder;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+public class IndexFragment extends Fragment {
+    private RecyclerView recyclerView;
+    private SchemeAdapter schemeAdapter;
+    private SchemeViewModel schemeViewModel;
+    private LiveData<List<Scheme>> schemeList;
+
+    @Override
+    public View onCreateView(
+            LayoutInflater inflater, ViewGroup container,
+            Bundle savedInstanceState
+    ) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_index, container, false);
+    }
+
+    public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        getActivity().findViewById(R.id.toolbar).setVisibility(View.VISIBLE);
+        getActivity().findViewById(R.id.fab).setVisibility(View.VISIBLE);
+        getActivity().findViewById(R.id.fab).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                NavHostFragment.findNavController(IndexFragment.this)
+                        .navigate(R.id.action_FirstFragment_to_SecondFragment);
+
+            }
+        });
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        schemeViewModel = new ViewModelProvider(getActivity()).get(SchemeViewModel.class);
+        recyclerView = getActivity().findViewById(R.id.recyclerView);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+        schemeAdapter = new SchemeAdapter(schemeViewModel);
+        //todo 接下来要怎么做？
+        Scheme scheme = new Scheme("AMD游戏平台", 36.2, "r7-3800x");
+        schemeViewModel.insertSchemes(scheme);
+        schemeViewModel.getAllScheme().observe(getViewLifecycleOwner(), new Observer<List<Scheme>>() {
+            @Override
+            public void onChanged(List<Scheme> schemes) {
+                schemeAdapter.setSchemes(schemes);
+            }
+        });
+        recyclerView.setAdapter(schemeAdapter);
+    }
+}
