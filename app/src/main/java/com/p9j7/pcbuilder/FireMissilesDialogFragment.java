6ebe44e760f2f2dcd839b832587b4aea8957b6ad
Ynight <vae1986s>
2020-04-23 17:09:32 +0800

    首页的功能完成

---
+++ app/src/main/java/com/p9j7/pcbuilder/FireMissilesDialogFragment.java
@@ -0,0 +1,27 @@
+package com.p9j7.pcbuilder;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.fragment.app.DialogFragment;
+
+
+public class FireMissilesDialogFragment extends DialogFragment {
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        // Use the Builder class for convenient dialog construction
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        builder.setTitle("PC Builder 1.0")
+                .setView(requireActivity().getLayoutInflater().inflate(R.layout.dialog_about, null))
+                .setPositiveButton(R.string.fire, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        // FIRE ZE MISSILES!
+                    }
+                });
+        // Create the AlertDialog object and return it
+        return builder.create();
+    }
+}
